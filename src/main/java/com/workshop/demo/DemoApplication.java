480894cd9a89d9e2da7cac3d672d95fce6e5ddff
GANIDU2413 <ganidusahan@gmail.com>
2024-02-22 10:06:45 +0530

    Initial commit

---
+++ src/main/java/com/workshop/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.workshop.demo;
+
+import com.workshop.demo.module.Laptop;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		Laptop laptop = new Laptop();
+		laptop.compile();
+	}
+
+}
---
+++ src/main/java/com/workshop/demo/module/Laptop.java
@@ -0,0 +1,7 @@
+package com.workshop.demo.module;
+
+public class Laptop {
+    public void compile(){
+        System.out.println("Laptop Compiling....");
+    }
+}
